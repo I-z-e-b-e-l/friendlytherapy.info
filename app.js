4b775bf9b61fed5760ce87906caf5a1095bdcbf7
izebel <izebel@rocketmail.com>
2021-05-28 12:11:51 -0400

    update backend: start ejs

--- app.js
+++ app.js
@@ -3,7 +3,11 @@ const app=express();
 const morgan = require('morgan');
 const PORT = 3000;
 
+const path = require('path');
 
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.set('view engine', 'ejs');
 
 
 app.listen(PORT, () => {
